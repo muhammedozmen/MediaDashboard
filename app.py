55a9d150193c9c59f3568f8a2407cbc6bd5344eb
Muhammed Ozmen <m.ozmenn34@gmail.com>
2024-08-27 14:17:10 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,117 @@
+import streamlit as st
+import plotly.express as px
+import pandas as pd
+from collections import Counter
+from itertools import combinations
+
+import cloudinary_service
+
+
+@st.cache_data
+def get_images_with_tags():
+    return cloudinary_service.get_all_images_with_tags()
+
+all_images = get_images_with_tags()
+
+all_tags = []
+all_tags_list = []
+for image in all_images:
+    tags = image['tags']
+    all_tags.extend(tags)
+    all_tags_list.append(tags)
+
+tag_counter = Counter(all_tags)
+
+# print(tag_counter)
+# print(all_tags_list)
+sorted_tags = [item for item in sorted(tag_counter.items(), key=lambda x: -x[1])]
+sorted_tag_strings = [f"{item[0]} ({item[1]})" for item in sorted_tags]
+
+combs = []
+for tag_per_image in all_tags_list:
+    for comb in combinations(tag_per_image, 2):
+        combs.append(comb)
+
+most_common_combs = Counter(combs).most_common(20)
+
+
+def show_images(images):
+    columns = st.columns(3)
+    for idx, img in enumerate(images):
+        col = columns[idx % 3]
+        url = img['url']
+        if url.endswith(".heic"):
+            url = url[:-5] + ".jpg"
+        with col:
+            st.image(url)
+            st.markdown(f"[Link]({url})")
+
+
+def image_page():
+    options = sorted_tag_strings[:20]
+    for item in most_common_combs:
+        options.append(f"{item[0][0]}, {item[0][1]} ({item[1]})")
+
+    tag = st.selectbox("Select Tag", options)
+
+    idx = tag.find("(")
+    tag = tag[:idx-1]
+    if "," in tag:
+        tag1, tag2 = tag.split(",")
+        tag1, tag2 = tag1.strip(), tag2.strip()
+
+        images_with_tag = [img for img in all_images if tag1 in img["tags"] and tag2 in img["tags"]]
+    else:
+        images_with_tag = [img for img in all_images if tag in img["tags"]]
+    
+    show_images(images_with_tag)
+
+
+def stats_page(min_tags_number=3):
+    filtered_tags = {
+        k: v
+        for k, v in sorted(tag_counter.items(), key=lambda x: -x[1])
+        if v >= min_tags_number
+    }
+
+    labels = list(filtered_tags.keys())
+    counts = list(filtered_tags.values())
+
+
+    st.markdown(f"#### Top {min_tags_number} Tags")
+
+    df = pd.DataFrame(list(zip(labels, counts)), columns=["Tags", "Counts"])
+    fig = px.pie(df, values="Counts", names="Tags")
+    fig.update_traces(textinfo="label+percent")
+    fig.update_layout(width=700, height=700)
+    st.plotly_chart(fig)
+
+
+    st.markdown(f"#### Top 3 Tags")
+
+    df = pd.DataFrame(list(zip(labels[:3], counts[:3])), columns=["Tags", "Counts"])
+    fig = px.bar(df, x="Tags", y="Counts")
+    fig.update_layout(width=800, height=500)
+    st.plotly_chart(fig)
+
+
+    st.markdown(f"#### Most common combinations")
+
+    labels = [str(x[0]) for x in most_common_combs][::-1]
+    counts = [x[1] for x in most_common_combs][::-1]
+    df = pd.DataFrame(list(zip(labels, counts)), columns=["Combinations", "Counts"])
+    fig = px.bar(df, x="Counts", y="Combinations", orientation="h")
+    fig.update_layout(width=800, height=600)
+    st.plotly_chart(fig)
+
+
+if __name__ == "__main__":
+    options = ("Image Gallery", "Image Stats")
+    selection = st.sidebar.selectbox("Menu", options)
+
+    if selection == "Image Gallery":
+        st.title("Image Gallery")
+        image_page()
+    else:
+        st.title("Image Stats")
+        stats_page()
---
+++ cloudinary_service.py
@@ -0,0 +1,94 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
+import pathlib
+import cloudinary
+import cloudinary.search
+from cloudinary import CloudinaryImage
+import cloudinary.uploader
+import cloudinary.api
+
+config = cloudinary.config(secure=True)
+supported_files = (".png", ".jpg", ".jpeg", ".heic")
+
+
+def upload_image(filename, folder="my_photos"):
+    stem = pathlib.Path(filename).stem
+    res = cloudinary.uploader.upload(filename, public_id = stem, folder=folder)
+    return res
+
+
+def upload_and_tag_image(filename, folder="my_photos"):
+    stem = pathlib.Path(filename).stem
+    res = cloudinary.uploader.upload(filename, public_id = stem,
+                                     folder=folder, detection = "openimages", auto_tagging = 0.25)
+    return res
+
+
+def upload_folder():
+    n = 0
+    for file in sorted(os.listdir("photos")):
+        if pathlib.Path(file).suffix.lower() in supported_files:
+            try:
+                print(file)
+                upload_and_tag_image("photos/" + file)
+                n += 1
+            except Exception as e:
+                print("failed for ", file)
+                print(e)
+    print(n, " photos uploaded")
+
+
+def get_all_tags():
+    all_tags = []
+    tags = cloudinary.api.tags(max_result = 100)
+    all_tags.extend(tags['tags'])
+    next_cursor = tags.get('next_cursor')
+
+    while next_cursor:
+        tags = cloudinary.api.tags(max_result = 100, next_cursor = next_cursor)
+        all_tags.extend(tags['tags'])
+        next_cursor = tags.get('next_cursor')
+    
+    return all_tags
+
+# all_tags = get_all_tags()
+# print(all_tags)
+
+
+def search_img():
+    result = cloudinary.Search().expression("resource_type:image AND tags=suit").sort_by("public_id", "desc").execute()
+    return result
+
+
+def get_all_images_with_tags():
+    all_resources = []
+    result = cloudinary.api.resources(
+        type = "upload",
+        resource_type = "image",
+        prefix = "my_photos",
+        tags = True,
+        max_result = 100
+    )
+    all_resources.extend(result['resources'])
+    next_cursor = result.get("next_cursor")
+
+    while next_cursor:
+        result = cloudinary.api.resources(
+            type = "upload",
+            resource_type = "image",
+            prefix = "my_photos",
+            tags = True,
+            max_result = 100,
+            next_cursor = next_cursor
+        )
+        all_resources.extend(result['resources'])
+        next_cursor = result.get("next_cursor")
+
+    return all_resources
+
+
+all_resources = get_all_images_with_tags()
+print(all_resources)
+print(len(all_resources))
